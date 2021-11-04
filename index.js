7df78d5e7462cf8713a13aa85d02ee141f8bc090
Noah Blumenstein <noah.blumenstein@gmail.com>
2021-11-04 12:13:20 -0400

    commit tree 809956be04a964d27bc2cee808b908693fe244ad

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+// function superbowlWin(array) {
+//     for (let item of array) {
+//         if (item.result === "W") {
+//             return item.year
+//         }
+//     }
+// }
+
+function superbowlWin(array) {
+    if (array.result === "W") {
+        return array.year;
+    }
+}
+
+let findWin = record.find(superbowlWin);
+findWin;
